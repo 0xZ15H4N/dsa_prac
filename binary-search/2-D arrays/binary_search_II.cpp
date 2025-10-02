202d49f03339c781fec9739f08a62b5c7686db26
zishan <zishanza436@gmail.com>
2025-10-02 16:11:17 +0530

    seraching in 2d matrix II done

--- binary-search/2-D arrays/binary_search_II.cpp
+++ binary-search/2-D arrays/binary_search_II.cpp
@@ -11,6 +11,46 @@ It should follow
 
 */
 
+/*
 
+To Solve this what we will do is 
+    first rowIndex -> 0
+          colIndex -> Nofcolumns - 1;
+          row = arr.size();
+          col = arr[0}.size();
+
+    while(rowIndex < row && colIndex >=0)
+    if(target) > arr[rowIndex}[colIndex} -> rowIndex++;
+    else colIndex --;
+    with this we will decrease our search space with each iteration
+
+*/
+
+
+int main(){
+
+    vector<vector<int>> arr = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+    int row = arr.size();
+    int col = arr[0].size();
+
+    int rowIndex = 0;
+    int colIndex = col - 1;
+    int target = 34;
+
+    while(rowIndex < row && colIndex >=0){
+        int ele = arr[rowIndex][colIndex];
+        if(ele == target){
+            printf("The element found at arr[%d][%d]",rowIndex,colIndex);
+            return 0;
+        }
+        if(ele > target){
+            colIndex --;
+        }else{
+            rowIndex++;
+        }
+    }
+    cout<<"element not found !";
+    return 0;
+}
 
 
