fcb46d47e35c80a8a57922ce8acac32417941b93
zishan <zishanza436@gmail.com>
2026-04-14 14:05:06 +0530

    restructured

---
+++ recursion/easy/check_palindrome.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Now we are checkin the given string is palindrome or not
+*/
+
+bool check_palindrome(string str,int i ,int j){
+
+    if(i>j){
+        return true;
+    }
+    if(str[i] == ' '){
+        i++;
+    }
+    if(str[j] == ' '){
+        j--;
+    }
+    if(str[i] == str[j]){
+        return check_palindrome(str,++i,--j);
+    }
+    else{
+        return false;
+    }
+}
+
+
+int main(){
+
+    string str ;
+    getline(cin,str);
+    cout<<check_palindrome(str,0,str.size()-1);
+
+    return 0;
+}
